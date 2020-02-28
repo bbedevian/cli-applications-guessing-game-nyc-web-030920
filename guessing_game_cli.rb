405e3d386b43ccff6cd930c4751a1ca085ca36cc
Brett Bedevian <brettbedevian@gmail.com>
2020-02-28 17:30:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game 
   random_num = rand(6) + 1
   # puts "Guess a number between 1 and 6"
-  input = gets
+  input = gets.to_i
   if input == random_num
     puts "You guessed the correct number!"
   elsif input == "exit"
